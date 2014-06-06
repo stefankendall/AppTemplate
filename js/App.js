a49d05c4bbd1d2bbf3288d7cdc88fc1c6f143ce3
Stefan Kendall <stefankendall@gmail.com>
2014-06-06 13:53:52 -0400

    copy of example

--- js/App.js
+++ js/App.js
@@ -2,13 +2,61 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
     items: [
-        {
-            html: 'test'
-        }
     ],
     
     launch: function() {
-        
+        Ext.create('Rally.data.wsapi.Store', {
+            model: 'User',
+            autoLoad: true,
+            filters: [
+                {
+                    property: 'TeamMemberships',
+                    operator: 'contains',
+                    value: Rally.util.Ref.getRelativeUri(this.getContext().getProject())
+                }
+            ],
+            listeners: {
+                load: this._onTeamMembersLoaded,
+                scope: this
+            }
+        }); 
+    },
+
+    _onTeamMembersLoaded: function(store, records) {
+        var columns = [
+            {
+                value: null,
+                columnHeaderConfig: {
+                    headerData: {owner: 'No Owner'}
+                }
+            }
+        ];
+
+        _.each(records, function(record) {
+            columns.push({
+                value: record.getRef().getRelativeUri(),
+                columnHeaderConfig: {
+                    headerData: {owner: record.get('_refObjectName')}
+                }
+            });
+        });
+
+        this._addBoard(columns);
+    },
+
+    _addBoard: function(columns) {
+        this.add({
+            xtype: 'rallycardboard',
+            types: ['User Story'],
+            attribute: 'Owner',
+            context: this.getContext(),
+            columnConfig: {
+                columnHeaderConfig: {
+                    headerTpl: '{owner}'
+                }
+            },
+            columns: columns
+        });
     }
     
 });
