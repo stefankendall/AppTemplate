26f6ef1601ed68dd71a774c0af90a32f9e3c3cd1
Stefan Kendall <stefankendall@gmail.com>
2014-06-06 11:40:24 -0400

    Base project template

---
+++ js/App.js
@@ -0,0 +1,14 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    items: [
+        {
+            html: 'test'
+        }
+    ],
+    
+    launch: function() {
+        
+    }
+    
+});
